5ef85e229a1fc4219c555ac1d31dbb8ca3db0280
nivs <nivslive@gmail.com>
2023-02-26 14:12:02 -0300

    atts

--- routes/Web/Posts.php
+++ routes/Web/Posts.php
@@ -1,7 +1,9 @@
 <?php
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{ChatController, SubjectController, MessageController};
+use App\Http\Controllers\ChatController;
+use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\MessageController;
 
 Route::get('/room/post', [ChatController::class, 'post'])->name('room.post');
 Route::get('/subject/post', [SubjectController::class, 'post'])->name('subject.post');
