05de15e7102b6c88e598ea185635949180fe7174
nivs <brenddaolveira@gmail.com>
2023-03-26 09:19:13 -0300

    test

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::controller(ThemeController::class)->group(function() {
+
+});
