7b213e03cf558390c2c338cf83c84a54a783365d
Nivan Jr <78574377+nivslive@users.noreply.github.com>
2023-03-29 00:54:40 -0300

    ATTS

---
+++ Modules/Social/[WIP]Theme/Database/Migrations/2023_03_27_175724_create_themes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('themes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            #foreign subject
+            $table->string('title');
+            $table->string('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reactions');
+    }
+};
