ee980a7f7368dd28134175bba06533343af7ec63
Nivs <nivsoficial@gmail.com>
2023-03-29 23:02:42 -0300

    ATTS

--- Laravel/app/Console/Commands/MakeModules.php
+++ Laravel/app/Console/Commands/MakeModules.php
@@ -11,7 +11,7 @@ class MakeModules extends Command
      *
      * @var string
      */
-    protected $signature = 'make:module';
+    protected $signature = 'make:module {--group=} {--controller} {--model} {--factory}';
 
     /**
      * The console command description.
@@ -25,11 +25,35 @@ class MakeModules extends Command
      *
      * @return mixed
      */
+
+    public function mkdirIfNotExists($dir) {
+        if(!file_exists($dir)) {
+            return mkdir($dir);
+        }
+        return;
+    }
     public function handle()
     {
-        $namespace = $this->ask('Qual é o nome do grupo de modulos?');
+        if($this->option('group')) {
+            $namespace = $this->option('group');
+        }
+        else {
+            $namespace = $this->ask('Qual é o nome do grupo de modulos?');
+        }
 
-        $modules = $this->ask('Quais módulos você gostaria de criar? (separe os nomes por espaço)');
+        if(!$this->option('controller')) {
+            $modules = $this->ask('Quais módulos você gostaria de criar? (separe os nomes por espaço)');
+        } else {
+            $part = $this->ask('Qual parte do módulo você gostaria de utilizar para criar o controller');
+            $controller = $this->ask('Qual o nome do controller?');
+            $moduleDir = base_path("../Modules/{$namespace}/{$part}");
+            $controllerModuleWord = ucwords($controller);
+            $controllerPath = "{$moduleDir}/Controllers/{$controllerModuleWord}Controller.php";
+            $fileContents = file_get_contents(__DIR__."/Templates/Controller.txt");
+            touch($controllerPath);
+            file_put_contents($controllerPath, $fileContents);
+            return;
+        }
 
         $modules = explode(' ', $modules);
 
@@ -37,19 +61,43 @@ class MakeModules extends Command
             $moduleDir = base_path("../Modules/{$namespace}/{$module}");
             if (!file_exists($moduleDir)) {
                 mkdir($moduleDir, 0777, true);
-                mkdir("{$moduleDir}/Controllers");
-                $controllerModuleWord = ucwords($module);
-                touch("{$moduleDir}/Controllers/{$controllerModuleWord}Controller.php");
+
+                if(!$this->option('controller')):
+                    #CONTROLLER (mas fui burro e fiz a regra das rotas)
+                    $this->mkdirIfNotExists("{$moduleDir}/Controllers");
+                    $controllerModuleWord = ucwords($module);
+                    $controllerPath = "{$moduleDir}/Controllers/{$controllerModuleWord}Controller.php";
+                    $fileContents = file_get_contents(__DIR__."/Templates/Controller.txt");
+                    touch($controllerPath);
+                    $fileContents = str_replace('{MODULEGROUP}', $namespace, $fileContents);
+                    $fileContents = str_replace('{MODULE}', $controllerModuleWord, $fileContents);
+                    $fileContents = str_replace('{ENTITY}', ucwords($module), $fileContents);
+                    $fileContents = str_replace('{CONTROLLER}', ucwords($module) . 'Controller', $fileContents);
+                    $fileContents = str_replace('{PATH1}', $module, $fileContents);
+                    file_put_contents($controllerPath, $fileContents);
+                endif;
+
+
+
+
+                #MODELS
                 mkdir("{$moduleDir}/Models");
-                touch("{$moduleDir}/Models/Provider.php");
                 $modelModuleWord = ucwords($module);
-                touch("{$moduleDir}/Controllers/{$modelModuleWord}.php");
+                touch("{$moduleDir}/Models/{$modelModuleWord}.php");
+
+
+                #MIGRATIONS
                 mkdir("{$moduleDir}/Migrations");
+                $migrationModuleWord = ucwords($module);
+
+
+                #PRINCIPAL PROVIDER
                 touch("{$moduleDir}/Provider.php");
                 $fileContents = file_get_contents(__DIR__."/Templates/Provider.txt");
                 $fileContents = str_replace('{MODULEGROUP}', $namespace, $fileContents);
                 $fileContents = str_replace('{MODULE}', $modelModuleWord, $fileContents);
                 file_put_contents("{$moduleDir}/Provider.php", $fileContents);
+
             }
         }
     }
