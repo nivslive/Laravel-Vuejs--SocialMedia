dbb89b350452d27bc43da67a700dc7405ef326bd
nivs <nivs@xlab.tec.br>
2023-04-07 13:29:12 -0300

    atts

--- Laravel/resources/js/Pages/Welcome.vue
+++ Laravel/resources/js/Pages/Welcome.vue
@@ -381,8 +381,6 @@
                 </a>
             </div>
 
-            <!--<a :href="`${route('subject.post')}`">POST SUBJECT</a>
-      <a :href="`${route('message.post')}`">POST MESSAGE</a>-->
             <div
                 class="relative overflow-x-auto sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
 
