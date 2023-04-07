5cc66980768e3c0f31d1e88b81da7e62896239c9
nivs <nivs@xlab.tec.br>
2023-04-07 16:13:03 -0300

    atts

--- Laravel/resources/views/social/dashboard.blade.php
+++ Laravel/resources/views/social/dashboard.blade.php
@@ -9,8 +9,8 @@
     <link href="img/favicon.ico" rel="icon">
 
     <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&amp;display=swap" rel="stylesheet">   
+
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
@@ -40,8 +40,8 @@
         </div>
         <div class="row align-items-center py-2 px-lg-5">
             <div class="col-lg-4">
-                <a href="" class="navbar-brand d-none d-lg-block">
-                    <h1 class="m-0 display-5 text-uppercase"><span class="text-primary">News</span>Room</h1>
+                <a href="" class="navbar-brand d-none d-flex align-items-end">
+                    <h1 class="m-0 display-5 text-uppercase"><span class="text-primary">YORUS</span></h1>.club
                 </a>
             </div>
             <div class="col-lg-8 text-center text-lg-right">
@@ -129,9 +129,9 @@
             <div class="row">
                 <div class="col-lg-8">
                     <div class="owl-carousel owl-carousel-2 carousel-item-1 position-relative mb-3 mb-lg-0">
-                        <div class="position-relative overflow-hidden" style="height: 435px;">
+                        <div class="position-relative overflow-hidden b-radius m-3" style="height: 435px;">
                             <img class="img-fluid h-100" src="img/news-700x435-1.jpg" style="object-fit: cover;">
-                            <div class="overlay">
+                            <div class="overlay b-radius">
                                 <div class="mb-1">
                                     <a class="text-white" href="">Technology</a>
                                     <span class="px-2 text-white">/</span>
@@ -140,7 +140,7 @@
                                 <a class="h2 m-0 text-white font-weight-bold" href="">Sanctus amet sed amet ipsum lorem. Dolores et erat et elitr sea sed</a>
                             </div>
                         </div>
-                        <div class="position-relative overflow-hidden" style="height: 435px;">
+                        <div class="position-relative overflow-hidden b-radius m-3" style="height: 435px;">
                             <img class="img-fluid h-100" src="img/news-700x435-2.jpg" style="object-fit: cover;">
                             <div class="overlay">
                                 <div class="mb-1">
@@ -154,29 +154,29 @@
                     </div>
                 </div>
                 <div class="col-lg-4">
-                    <div class="d-flex align-items-center justify-content-between bg-light py-2 px-4 mb-3">
+                    <div class="d-flex align-items-center justify-content-between bg-light py-2 px-4 mb-3 b-radius">
                         <h3 class="m-0">Categories</h3>
                         <a class="text-secondary font-weight-medium text-decoration-none" href="">View All</a>
                     </div>
-                    <div class="position-relative overflow-hidden mb-3" style="height: 80px;">
+                    <div class="position-relative overflow-hidden mb-3 b-radius" style="height: 80px;">
                         <img class="img-fluid w-100 h-100" src="img/cat-500x80-1.jpg" style="object-fit: cover;">
-                        <a href="" class="overlay align-items-center justify-content-center h4 m-0 text-white text-decoration-none">
+                        <a href="" class=" overlay align-items-center justify-content-center h4 m-0 text-white text-decoration-none">
                             Business
                         </a>
                     </div>
-                    <div class="position-relative overflow-hidden mb-3" style="height: 80px;">
+                    <div class="position-relative overflow-hidden mb-3 b-radius" style="height: 80px;">
                         <img class="img-fluid w-100 h-100" src="img/cat-500x80-2.jpg" style="object-fit: cover;">
                         <a href="" class="overlay align-items-center justify-content-center h4 m-0 text-white text-decoration-none">
                             Technology
                         </a>
                     </div>
-                    <div class="position-relative overflow-hidden mb-3" style="height: 80px;">
+                    <div class="position-relative overflow-hidden mb-3 b-radius" style="height: 80px;">
                         <img class="img-fluid w-100 h-100" src="img/cat-500x80-3.jpg" style="object-fit: cover;">
                         <a href="" class="overlay align-items-center justify-content-center h4 m-0 text-white text-decoration-none">
                             Entertainment
                         </a>
                     </div>
-                    <div class="position-relative overflow-hidden" style="height: 80px;">
+                    <div class="position-relative overflow-hidden b-radius" style="height: 80px;">
                         <img class="img-fluid w-100 h-100" src="img/cat-500x80-4.jpg" style="object-fit: cover;">
                         <a href="" class="overlay align-items-center justify-content-center h4 m-0 text-white text-decoration-none">
                             Sports
@@ -779,7 +779,7 @@
                 </div>
             </div>
             <div class="col-lg-3 col-md-6 mb-5">
-                <h4 class="font-weight-bold mb-4">Categories</h4>
+                <h4 class="font-weight-bold mb-4 b-radius">Categories</h4>
                 <div class="d-flex flex-wrap m-n1">
                     <a href="" class="btn btn-sm btn-outline-secondary m-1">Politics</a>
                     <a href="" class="btn btn-sm btn-outline-secondary m-1">Business</a>
